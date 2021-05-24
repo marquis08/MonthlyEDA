68c1b5ae3b529b6331c7aabedcd13ad422bd5b92
marquis08 <roal214@naver.com>
2021-05-24 23:11:01 +0900

    fix: diable dotenv, use txt.file for secretkey

--- Main/Main/settings.py
+++ Main/Main/settings.py
@@ -20,10 +20,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 
-load_dotenv()
-SECRET_KEY = str(os.getenv('SECRET_KEY'))
+# load_dotenv()
+# SECRET_KEY = str(os.getenv('SECRET_KEY'))
+secret_file = os.path.join(BASE_DIR, 'key.txt')
+with open(secret_file, "r") as f:
+    mykey = f.readlines()
+    
+SECRET_KEY = mykey
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
